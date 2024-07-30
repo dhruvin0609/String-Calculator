7f24f592a99144b5a511f33750afe49005e3bc44
Dhruvin Pambhar <dhruvinpambhar0609@gmail.com>
2024-07-30 09:04:43 +0530

    Add test case  for add numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,3 +5,14 @@ const add = require('./stringCalculator');
 test('returns 0 for an empty string', () => {
     expect(add('')).toBe(0);
 });
+// test case for checking sum of only 1 number
+test('returns the number itself for a single number', () => {
+    expect(add('1')).toBe(1);
+    expect(add('5')).toBe(5);
+});
+
+// test case for checking sum of 2 numbers
+test('returns the sum of two comma-separated numbers', () => {
+    expect(add('1,2')).toBe(3);
+    expect(add('4,5')).toBe(9);
+});
